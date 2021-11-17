08a5d2a7844682bf53db0146f0d5f690f97222ff
Sat Garcia <sat@sandiego.edu>
2021-11-16 18:57:03 -0800

    update so that convolution modifies a copy of the image rather than the original image

--- comp110_lab13.py
+++ comp110_lab13.py
@@ -7,19 +7,20 @@ Starter code for Lab 13 (Image convolution)
 import comp110_image
 
 
-def apply_kernel(img, img_copy, x, y, kernel):
+def apply_kernel(img, filtered_img, x, y, kernel):
     """
     Applies the given kernel to the pixel in img at (x,y).
 
     Params:
-    img (type: Picture) - The picture to modify
-    img_copy (type: Picture) - An unmodified version of img (used for reading
-        values)
+    img (type: Picture) - The original (unmodified) image.
+    filtered_img (type: Picture) - A copy of the original that will have the
+        kernel applied to it.
     x (type: int) - The x value of the pixel to modify
     y (type: int) - The y value of the pixel to modify
     kernel (type: 2D list of int) - The kernel to apply.
     """
 
+    # accumulator variables
     red_sum = 0
     green_sum = 0
     blue_sum = 0
@@ -28,17 +29,20 @@ def apply_kernel(img, img_copy, x, y, kernel):
         for j in range(-1, 2):
             # To do: Get the correct neighborhood pixel
 
-            # To do: add to red_sum, green_sum, and blue_sum based on
+            # To Do: add to red_sum, green_sum, and blue_sum based on
             # multiplication the pixel's color with the kernel.
-            # Note that you will use getPixel to get the pixel from img_copy
+            # Note that you will use getPixel to get the pixel from img
             # and kernel[???][???] to get the value in the kernel.
             pass
 
-    # To do: set the red, green, and blue components of the pixel at (x,y) to
-    # the sums we calculated.
-    # IMPORTANT: Before setting the color, use if statements to see if the
-    # red, green, or blue sum are out of range and update the value of each
-    # accordingly (e.g. set to 0 if the sum is less than 0.)
+    # To Do: If red_sum, green_sum, or blue_sum are outside of the correct
+    # range, set them to be within the range. For example, if red_sum is less
+    # than 0, change it to 0.
+    # For this step, you'll need to have three consecutive chained conditionals
+    # (one for each of the color components).
+
+    # To Do: Update the pixel at (x, y) in filtered_img to change the red,
+    # green, and blue components to the sums we calculated.
 
 
 def convolution(img, kernel):
@@ -50,7 +54,10 @@ def convolution(img, kernel):
     img (type: Picture) - The picture to modify.
     kernel (type: 2D list of int) - The kernel to apply.
     """
-    img_copy = img.copy()
+
+    # Make a copy of the original image. This copy will be modified while the
+    # original will remain unchanged.
+    filtered_img = img.copy()
 
     # To Do: modify range to avoid border pixels
     for x in range(img.getWidth()):
@@ -58,15 +65,17 @@ def convolution(img, kernel):
             # To Do: call the apply_kernel function here
             pass
 
+    return filtered_img
+
 def main():
     # Do not modify this function
 
     cat_img = comp110_image.Picture(filename="cute-cat.jpg")
     cat_img.setTitle("Before convolution (edge detect)")
     cat_img.show()
-    convolution(cat_img, [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-    cat_img.setTitle("After convolution (edge detect)")
-    cat_img.show()
+    convoluted_cat = convolution(cat_img, [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    convoluted_cat.setTitle("After convolution (edge detect)")
+    convoluted_cat.show()
 
 
 """ DO NOT MODIFY ANYTHING BELOW THIS LINE! """
